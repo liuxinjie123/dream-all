a1a4fbfa6aa0ae6ab8a5ceab0028005f224c212c
liuxinjie123 <jackcodejs@gmail.com>
2018-04-23 23:46:04 +0800

    dubbo

---
+++ dream-api/src/main/java/com/dream/api/user/UserDubboTestService.java
@@ -0,0 +1,6 @@
+package com.dream.api.user;
+
+public interface UserDubboTestService {
+
+    String helloDubbo();
+}
---
+++ dream-service/src/main/java/com/dream/impl/user/UserDubboTestServiceImpl.java
@@ -0,0 +1,12 @@
+package com.dream.impl.user;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.dream.api.user.UserDubboTestService;
+
+@Service(version = "1.0.0")
+public class UserDubboTestServiceImpl implements UserDubboTestService {
+    @Override
+    public String helloDubbo() {
+        return "Hello Dubbo";
+    }
+}
